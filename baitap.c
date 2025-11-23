15ecafcc0403e2a02057439892bb91ba7ce865b2
thanhxuan090907-rgb <thanhxuan090907@gmail.com>
2025-11-23 23:21:32 +0700

    first commit

--- baitap.c
+++ baitap.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
-int main() {
-    for (int i = 1; i <= 15; i++) {
-        printf ("%d\n", i);
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+    int so_bi_mat;
+    int so_doan = 0;
+    int so_lan_doan = 0;
+    srand (time (0));
+    so_bi_mat = rand() % 100 + 1;
+
+    printf ("----- TRO CHOI DOAN SO (1 den 100) -----\n");
+    printf ("Hay doan so bi mat!\n");
+
+    while (so_doan != so_bi_mat) {
+        printf ("Nhap so ban doan: ");
+        scanf ("%d", &so_doan);
+        so_lan_doan++;
+
+        if (so_doan > so_bi_mat) {
+            printf("So cua ban lon hon so bi mat! Xin vui  long thu lai.");
+        }else if (so_doan < so_bi_mat) {
+            printf ("So cua ban nho hon so bi mat! Vui long thu lai.");
+        }else {
+            printf ("XIN CHUC MUNG! Ban da doan dung so %d\n", so_bi_mat);
+            printf ("Ban da mat %d lan doan de chien thang.\n", so_lan_doan);
+
+        }
+        return 0;
 
     }
-    return 0;
+
+
 }
